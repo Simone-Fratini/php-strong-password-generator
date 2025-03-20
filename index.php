614e7c04c0961fee5813e6d3e93e048413bd92b8
Simone-Fratini <simone@fratini.com>
2025-03-20 18:40:46 +0100

    Update index.php: Made password length input required, updated checkbox names for character types, and adjusted button types for form submission.

--- index.php
+++ index.php
@@ -22,7 +22,7 @@
                 <form action="" class="mt-5">
                     <div>
                         <label for="length">1. Lunghezza password</label>
-                        <input type="number" name="length" id="length" class="bg-white rounded text-black w-20 ml-5">
+                        <input type="number" name="length" id="length" class="bg-white rounded text-black w-20 ml-5" required>
                     </div>
                     
                     <div class="mt-2">
@@ -33,21 +33,21 @@
                     <div class="mt-2">
                         <div>3. Quali caratteri vuoi:</div>
                         <div class="mt-2 ml-4">
-                            <input type="checkbox" name="filter" id="filter">
-                            <label for="filter">Lettere</label>
+                            <input type="checkbox" name="letters" id="letters">
+                            <label for="letters">Lettere</label>
                         </div>
                         <div class="ml-4">
-                            <input type="checkbox" name="filter" id="filter">
-                            <label for="filter">Numeri</label>
+                            <input type="checkbox" name="numbers" id="numbers">
+                            <label for="numbers">Numeri</label>
                         </div>
                         <div class="ml-4">
-                            <input type="checkbox" name="filter" id="filter">
-                            <label for="filter">Simboli</label>
+                            <input type="checkbox" name="simbols" id="simbols">
+                            <label for="simbols">Simboli</label>
                         </div>
 
                     </div>
-                    <button class="bg-green-700 rounded px-2 py-1 cursor-pointer mt-5 hover:bg-green-500 delay-100 transition-all">Genera</button>
-                    <button class="border rounded px-2 py-1 cursor-pointer hover:bg-red-600 delay-100 transition-all">Reset</button>
+                    <button type="submit" class="bg-green-700 rounded px-2 py-1 cursor-pointer mt-5 hover:bg-green-500 delay-100 transition-all">Genera</button>
+                    <button submit class="border rounded px-2 py-1 cursor-pointer hover:bg-red-600 delay-100 transition-all">Reset</button>
                     
                 </form>
             </div>
