8bc54133951162b344f6c50553b097abeff18e72
Simone-Fratini <simone@fratini.com>
2025-03-21 01:32:41 +0100

    Add functions.php: Implement password generation function
    Created a new file, functions.php, containing the generatePassword function. This function generates a password based on specified criteria including character types and repetition options, enhancing the overall password generation functionality.

--- index.php
+++ index.php
@@ -25,57 +25,7 @@ if(isset($_GET["simbols"]) && $_GET["simbols"] == "on"){
     $simbolsFilter = true;
 }
 
-
-function generatePassword($repetition, $length, $letters, $numbers, $simbols){
-    
-    if (!$letters && !$numbers && !$simbols) {
-        return "Errore: seleziona almeno un tipo di carattere";
-    }
-    
-    $generatedPassword = "";
-    $generatedLength = 0;
-
-    while($generatedLength < $length){
-        $randomNumber = rand(1,3);
-
-        if($randomNumber == 1 && $letters == true){
-            $tempVar = chr(rand(0, 9) > 5 ? rand(65, 90) : rand(97,122));
-            if($repetition == true){
-                $generatedPassword .= $tempVar;
-                $generatedLength++;
-            }else{
-                if(strpos($generatedPassword, $tempVar) === false){
-                    $generatedPassword .= $tempVar;
-                    $generatedLength++;
-                }
-            }
-        }elseif ($randomNumber == 2 && $numbers == true){
-            $tempVar = rand(0,9);
-            if($repetition == true){
-                $generatedPassword .= $tempVar;
-                $generatedLength++;
-            }else{
-                if(strpos($generatedPassword, (string)$tempVar) === false){
-                    $generatedPassword .= $tempVar;
-                    $generatedLength++;
-                }
-            }
-        }elseif ($randomNumber == 3 && $simbols == true){
-            $tempVar = chr(rand(33, 47));
-            if($repetition == true){
-                $generatedPassword .= $tempVar;
-                $generatedLength++;
-            }else{
-                if(strpos($generatedPassword, $tempVar) === false){
-                    $generatedPassword .= $tempVar;
-                    $generatedLength++;
-                }
-            }
-        }
-    }
-
-    return $generatedPassword;
-}
+require "./functions.php";
 ?>
 
 
